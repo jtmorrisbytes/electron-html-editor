1cfb9311011cc30f11381b837da007027850f3e0
Jordan Morris <jthecybertinkerer@gmail.com>
2019-12-27 20:19:40 -0600

    added basic argument checking for Layout Component

--- layout-manager/index.js
+++ layout-manager/index.js
@@ -14,17 +14,32 @@
 class Layout extends HTMLElement {
     // Need a container to put the layout in
     static tagName = 'layout'
-    type = "grid"
-
+    defaultLayout = "grid"
+    layout = defaultLayout
+    classDescString = `Class ${this.className}  `
     constructor(options = {}) {
         if (!options) {
             //if options were not passed into consructor
             // console.error(`Class ${this.className}`)
+            if (!this.hasAttribute("layoutType") && !this.type) {
+                console.error(this.classDescString.concat(` requires argument 'layoutType' `))
+            }
+            else if(this.hasAttribute("layoutType")){
+                // this will need validation later!
+                this.layout = this.getAttribute("layoutType")
+            }
         }
-        else if (options.type == 'grid') {
-            //grid layout
+        else if (options.type) {
+            switch(options.type) {
+                case "grid":
+                case "flex":
+                    this.layout = options.type
+                break;
+                default:
+                    console.error(this.classDescString.concat(`invalid layout type: '${String(options.type)}'`))
+                
+            }
         }
-        else if (options.type = '')
 
     }
     connectedCallback(){
@@ -32,5 +47,22 @@ class Layout extends HTMLElement {
     }
     disconnectedCallback(){
 
+    }
+}
+
+
+
+
+// add to module.exports if availible
+
+if((module || {}).exports) {
+    module.exports = Layout
+}
+else {
+    try {
+        export default Layout
+    }
+    catch {
+        //swallow error
     }
 }
